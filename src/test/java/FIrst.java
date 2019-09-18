838f4040499fc267b105712dacdfc85239fd7985
Alekseev Yaroslav <alekseevy509@gmail.com>
2019-09-19 02:24:09 +0300

    Add tests

---
+++ src/test/java/FIrst.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+//for testing
+public class FIrst {
+    @Test
+    public void firstTest() throws Exception {
+        Assert.assertEquals(5, 5);
+    }
+}
