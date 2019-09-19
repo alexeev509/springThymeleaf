8c73b180488da2b98ed42da9c257488fad8151cf
Alekseev Yaroslav <alekseevy509@gmail.com>
2019-09-19 18:05:01 +0300

    Add tests

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,9 +2,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 //for testing
-public class FIrst {
+public class FirstTest {
     @Test
     public void firstTest() throws Exception {
+        System.out.println("hii");
         Assert.assertEquals(5, 7);
     }
 }
